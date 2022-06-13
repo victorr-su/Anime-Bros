6652330cab6094a6d55033285889bd019d6a1018
Victor <su.victor21@gmail.com>
2022-06-13 17:15:48 -0400

    api updates

--- Api/index.js
+++ Api/index.js
@@ -4,20 +4,19 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRouter = require("./Routes/user");
 const authRouter = require("./Routes/auth");
+const productRouter = require("./Routes/product");
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL)
     .then(()=>console.log("db connection successful"))
     .catch((err)=>{console.log(err)})
-    app.get("/api/test", ()=>{
-        console.log("test is successful")
-    })
 
 // middleware
 app.use(express.json())
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
 
 app.listen(process.env.PORT || 5000, (req,res) =>{
     console.log("server is running");
