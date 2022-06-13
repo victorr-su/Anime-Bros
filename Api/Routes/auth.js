99ad70802acd2f4a206b76eb4c79567bde064dea
Victor <su.victor21@gmail.com>
2022-06-13 13:00:58 -0400

    api updates

--- Api/Routes/auth.js
+++ Api/Routes/auth.js
@@ -1,43 +1,78 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken"); 
 
 //REGISTER
 
 
 router.post("/register", async (req,res)=>{
     
-    if(Object.keys(req.body.username).length ===0 || Object.keys(req.body.email).length ===0 || Object.keys(req.body.password).length ===0 ) {
-        res.status(500)
-        res.send("Please fill in all parts of the form")
+    // Checks if all fields are empty
+    if(Object.keys(req.body.username).length === 0 || Object.keys(req.body.email).length === 0 || Object.keys(req.body.password).length === 0 ) {
+        res.status(500).json("Please fill in all parts of the form")
+        return;
     }
 
+    // Checks for duplicate username
+    const usernameCheck = await User.findOne({ username: req.body.username });
+    if(usernameCheck){
+        res.status(400).json("An Account with that username already exists, please choose a different one.");
+        return;
+    }
+
+    // Checcks for duplicate email
+    const emailCheck = await User.findOne({ email: req.body.email });
+    if(emailCheck){
+        res.status(400).json("An Account with that email already exists, please choose a different one.");
+        return;
+    }
+
+    // creates new user
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
         password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString()
     });
-    
+
     try{
         const savedUser = await newUser.save();
         res.status(201).json(savedUser);
     }catch(err){
-        res.status(500).json(err)
+        res.status(500).json("Error creating user, please try again or contact support")
     }
 });
+
 
 // LOGIN
 router.post("/login", async (req,res) =>{
     try{
-        const user = await new User.findOne({
-                username: req.body.username,
-                password: req.body.password
-            })
+        //Checks if user exists
+        const user = await User.findOne({ username: req.body.username });
+        if(!user){
+            res.status(401).json("Wrong Username!");
+        } 
+        //decrypts password and checks if it exists
+        const decodedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC).toString(CryptoJS.enc.Utf8);
+        if(decodedPassword !== req.body.password){
+            res.status(401).json("Wrong Password!");
+        }
+
+        const accessToken = jwt.sign(
+            {
+                id: user.id,
+                isAdmin: user.isAdmin
+            }, 
+            process.env.JWT_SEC, 
+            {expiresIn: "3d"}); 
+
+        const { password, ...other } = user._doc;
+
+        res.status(200).json({...other, accessToken});
+
     }catch(err){
-        res.status(500),json(err)
+        res.status(500).json(err)
     }
 });
-
-
 
 module.exports = router;
--- Api/Routes/user.js
+++ Api/Routes/user.js
@@ -1,4 +1,51 @@
+const User = require("../models/User");
+const {verifyToken, verifyTokenAndAuth, verifyTokenAndAdmin} = require("./verifyToken");
 const router = require("express").Router()
 
+//UPDATE
+router.put("/:id", verifyTokenAndAuth, async (req,res) =>{
+    if(req.body.password){
+        req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString()
+    }
+    try{
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, {new:true})
+        res.status(200).json(updatedUser)
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
+
+// DELETE
+router.delete("/:id", verifyTokenAndAuth, async (req,res)=>{
+    try{
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200),json("User has been deleted")
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
+
+//GET USER
+router.get("/find/:id", verifyTokenAndAdmin, async (req,res)=>{
+    try{
+        const user = await User.findById(req.params.id)
+        const { password, ...other } = user._doc;
+        res.status(200).json(other);
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
+
+//GET ALL USERS
+router.get("/", verifyTokenAndAdmin, async (req,res)=>{
+    try{
+        const users = await User.find()
+        res.status(200).json(users);
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
 
 module.exports = router;
